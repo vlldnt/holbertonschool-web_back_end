90172253450ed433ce297d7f8ae2470a3ea02053
vlldnt <vieilledent.adrien@gmail.com>
2025-03-26 16:41:52 +0100

    Task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
