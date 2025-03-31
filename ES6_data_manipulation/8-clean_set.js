6aaec0d016847b29386c6f4d7ca256f7d91bb87d
vlldnt <vieilledent.adrien@gmail.com>
2025-03-31 14:33:22 +0200

    Task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+    if (!startString)
+        return '';
+    return Array.from(set)
+        .filter(values => values.startsWith(startString))
+        .map(values => values
+        .slice(startString.length));
+};
