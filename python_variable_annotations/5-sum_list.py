edabe520ec3cf56a053addef9c6b16b02900ef41
vlldnt <vieilledent.adrien@gmail.com>
2025-04-22 14:41:16 +0200

    Task 4 5 6 7

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' Typed annotations sum_list'''
+
+
+def sum_list(input_list: list) -> float:
+    '''adding all float of the list'''
+    return sum(input_list)
---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+''' Type annoted sum_mixed_list'''
+
+
+def sum_mixed_list(mxd_lst: list) -> float:
+    return float(sum(mxd_lst))
