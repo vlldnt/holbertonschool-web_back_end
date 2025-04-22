a56527fc891131c0f73282631bfa1ad6d7ccc1d1
vlldnt <vieilledent.adrien@gmail.com>
2025-04-22 15:07:01 +0200

    6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Documentation mixed sum list'''
     return float(sum(mxd_lst))
