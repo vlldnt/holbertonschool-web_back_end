7bce2aedcc927d55e005d30c9f082b987bda1684
vlldnt <vieilledent.adrien@gmail.com>
2025-04-23 11:38:33 +0200

    update

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Module for measuring runtime"""
+
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    
