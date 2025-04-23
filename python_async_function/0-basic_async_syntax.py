a12b2673a6713a75c85555e2b54151803ce5bf2f
vlldnt <vieilledent.adrien@gmail.com>
2025-04-23 09:02:39 +0200

    docstrin g ?

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,11 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Sleep for few sec (float)'''
+    """
+    Method: wait_random - wait a rndm amount of time
+    Parameter: max_delay - a delay of up to 10 seconds
+    Return: the ammount of time of the delay
+    """
     wait = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
     return wait
