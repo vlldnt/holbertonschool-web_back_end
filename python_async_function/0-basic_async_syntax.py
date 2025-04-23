686ba3b604916b2823f546384c804663715cf6ba
vlldnt <vieilledent.adrien@gmail.com>
2025-04-23 09:09:37 +0200

    readme.md

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""
+'''
 function asynchronus wait a random delay and returns it in float value
-"""
+'''
 
 
 import random
@@ -9,11 +9,7 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """
-    Method: wait_random - wait a rndm amount of time
-    Parameter: max_delay - a delay of up to 10 seconds
-    Return: the ammount of time of the delay
-    """
+    '''wait a random of time'''
     wait: float = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
     return wait
