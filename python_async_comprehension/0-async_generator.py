2b9398011201d7ca8d1a29417a9e83b58bd5c2b3
vlldnt <vieilledent.adrien@gmail.com>
2025-04-28 11:34:42 +0200

    generator instead of asyncgenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Yield a float between 0 and 1O, ten times'''
     for _ in range(10):
         await asyncio.sleep(1)
