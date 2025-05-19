977df443a01cc4041988057434cad69ec02b9fda
vlldnt <vieilledent.adrien@gmail.com>
2025-05-19 13:26:28 +0200

    feat: task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        const students = lines.slice(1);
+        const fields = {};
+
+        students.forEach((line) => {
+          const [firstname, , , field] = line.split(',');
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        });
+
+        console.log(`Number of students: ${students.length}`);
+        Object.entries(fields).forEach(([field, list]) => {
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        });
+
+        resolve();
+      });
+    } catch (err) {
+      reject(new Error('Cannot load the database'));
+    }
+  });
+};
+
+module.exports = countStudents;
