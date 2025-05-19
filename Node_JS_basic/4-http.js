8eb4b1c1d9c167fbf80fc6a1b241b4ca7be43457
vlldnt <vieilledent.adrien@gmail.com>
2025-05-19 14:25:33 +0200

    style: eslint 4-http

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
-var http = require("http");
+var http = require('http');
 
-const app = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Hello Holberton School!");
-  res.end();
-}).listen(1245);
+const app = http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello Holberton School!');
+    res.end();
+  })
+  .listen(1245);
 
 module.exports = app;
