b54d1604cf4ac264151b420f0a40ae5e6ad3bfe9
vlldnt <vieilledent.adrien@gmail.com>
2025-05-19 14:23:11 +0200

    fix: adding const app

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-http.createServer(function(req, res) {
+const app = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("Hello Holberton School!");
   res.end();
