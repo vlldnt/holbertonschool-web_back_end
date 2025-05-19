b111b530a285e3775d6524e14960458197de5fd4
vlldnt <vieilledent.adrien@gmail.com>
2025-05-19 10:46:14 +0200

    fix: code correction for exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+
+process.stdin.on('readable', () => {
+  const word = process.stdin.read();
+  if (word) {
+    process.stdout.write(`Your name is: ${word.toString()}`);
+  }
 });
 
-process.on('SIGINT', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
