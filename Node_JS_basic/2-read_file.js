75ccbf60ac22a8fd119dc28b904d5ab10af1333a
vlldnt <vieilledent.adrien@gmail.com>
2025-05-19 11:48:19 +0200

    fix: task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line);
-    const fields = {};
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) {
+      console.log('Number of students: 0');
+      return;
+    }
+
     const students = lines.slice(1);
+    const fields = {};
 
     students.forEach((line) => {
-      const student = line.split(',');
-      const field = student[3];
+      const [firstname, , , field] = line.split(',');
       if (!fields[field]) fields[field] = [];
-      fields[field].push(student[0]);
+      fields[field].push(firstname);
     });
 
     console.log(`Number of students: ${students.length}`);
-    for (const field in fields) {
-      if (field) {
-        const list = fields[field].join(', ');
-        console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${list}`
-        );
-      }
-    }
+    Object.entries(fields).forEach(([field, list]) => {
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    });
   } catch (error) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
