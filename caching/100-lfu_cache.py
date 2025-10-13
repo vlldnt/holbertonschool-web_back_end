135afa58d834cc3e84ec5de7093202fce0c90da6
vlldnt <vieilledent.adrien@gmail.com>
2025-10-13 13:54:05 +0200

    feat: LFU

---
+++ caching/100-lfu_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from base_caching import BaseCaching
+
+class LFUCache(BaseCaching):
+    """Least Frequently Used Cache"""
+
+    def __init__(self):
+        super().__init__()
+        self.usage_count = {}
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_count[key] += 1
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            min_count = min(self.usage_count.values())
+            lfu_keys = [k for k, v in self.usage_count.items() if v == min_count]
+            lfu_key = lfu_keys[0]
+            print(f"DISCARD: {lfu_key}")
+            del self.cache_data[lfu_key]
+            del self.usage_count[lfu_key]
+
+        self.cache_data[key] = item
+        self.usage_count[key] = 1
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.usage_count[key] += 1
+        return self.cache_data[key]
