279e4a0b4879f9247f6579b23bc9202c06cfe3b4
vlldnt <vieilledent.adrien@gmail.com>
2025-10-13 10:47:22 +0200

    fix: correct docstring for BasicCache class and methods

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -8,14 +8,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    ''' Basic caching '''
+    '''
+    Basic caching classe
+    '''
 
     def put(self, key, item):
+        ''' put item value for key '''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        ''' get value from key '''
         if key is None:
             return None
         return self.cache_data.get(key)
