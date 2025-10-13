7e5d9f3e6d40568e0e5fe1e2d1899864dcccc50a
vlldnt <vieilledent.adrien@gmail.com>
2025-10-13 11:04:46 +0200

    feat: First In First Out caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+Create a class FIFOCache that inherits from BaseCaching and is a caching system
+'''
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        '''Super init'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''Add a new key with item, First_In First_Out'''
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            firstOut = next(iter(self.cache_data))
+            print(f"DISCARD: {firstOut}")
+            del self.cache_data[firstOut]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        ''' get value from key '''
+        if key is None:
+            return None
+        return self.cache_data.get(key)
