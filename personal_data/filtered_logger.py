d813e9da2ddfebf8278f1069b98cefdc81fc6b36
vlldnt <vieilledent.adrien@gmail.com>
2025-10-20 11:28:11 +0200

    feat: typing funtion

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     '''Change value by a new text format'''
     pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
     return re.sub(pattern, lambda f: f"{f.group(1)}={redaction}", message)
