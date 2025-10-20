f414e6ff0535e851cbb92c256cadaef23547561a
vlldnt <vieilledent.adrien@gmail.com>
2025-10-20 12:02:16 +0200

    feat: using logginf filter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,10 +4,32 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''Change value by a new text format'''
-    pattern = r'({})=([^{}]*)'.format('|'.join(fields), separator)
-    return re.sub(pattern, lambda f: f"{f.group(1)}={redaction}", message)
+    for field in fields:
+        message = re.sub(f"{field}=(.*?){separator}",
+                         f"{field}={redaction}{separator}",
+                         f"{message}")
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage()
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        return super().format(record)
