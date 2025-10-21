6ab951bcb0bb920b1304836c6100d55ad19e16de
vlldnt <vieilledent.adrien@gmail.com>
2025-10-21 10:24:23 +0200

    feat: check password if its matchs with hashed_password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,9 +7,14 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     '''encrypt password with bcrypt'''
-
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
 
     return (hash)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Check the validity of a password'''
+    bytes = password.encode('utf-8')
+    return bcrypt.checkpw(bytes, hashed_password)
