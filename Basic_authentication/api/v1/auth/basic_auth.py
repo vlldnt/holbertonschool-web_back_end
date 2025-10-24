2045ce7d26fc3e4476a17c524f0fbed41efec897
vlldnt <vieilledent.adrien@gmail.com>
2025-10-24 11:11:21 +0200

    feat: returns user instance

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,7 @@ for the API endpoints.
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -57,3 +58,21 @@ class BasicAuth(Auth):
         password = decoded_base64_authorization_header.split(':')[1]
 
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        '''Return user credentila as an object'''
+
+        if (user_email is None
+            or not isinstance(user_email, str)
+            or user_pwd is None
+            or not isinstance(user_pwd, str)):
+            return None
+
+        user = User.search({"email": user_email})
+
+        for cred in user:
+            if cred and cred.is_valid_password(user_pwd):
+                return cred
+
+        return None
