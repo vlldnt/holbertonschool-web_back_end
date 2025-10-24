a8af9909ec3734e76c249b5b0958cab3d787f208
vlldnt <vieilledent.adrien@gmail.com>
2025-10-24 10:10:51 +0200

    feat: extract user credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@ for the API endpoints.
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -41,3 +42,18 @@ class BasicAuth(Auth):
 
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> (str, str):
+        '''Extract user credentials method'''
+        if (decoded_base64_authorization_header is None
+                or not isinstance(decoded_base64_authorization_header, str)):
+            return None, None
+        elif ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email = decoded_base64_authorization_header.split(':')[0]
+        password = decoded_base64_authorization_header.split(':')[1]
+
+        return email, password
