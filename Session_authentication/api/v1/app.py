f205d08d8c475cb4dd73ceb79538da99fd6396f1
vlldnt <vieilledent.adrien@gmail.com>
2025-11-12 09:53:44 +0100

    pycodestyle:

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -70,7 +70,8 @@ def before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request):
+    if (auth.authorization_header(request)
+            and auth.session_cookie(request) is None):
         abort(401)
 
 
