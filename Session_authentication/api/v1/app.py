a1558a7ac5c66cb90671644d9bcdb9306cda2599
vlldnt <vieilledent.adrien@gmail.com>
2025-11-12 10:01:06 +0100

    fix: deleting two same method

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -64,9 +64,6 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
 
