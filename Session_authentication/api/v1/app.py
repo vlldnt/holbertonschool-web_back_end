52df296504b761f7f2de4abd21cbf74170f6eaa7
vlldnt <vieilledent.adrien@gmail.com>
2025-11-12 10:04:10 +0100

    fix: trying to find the rror

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -64,12 +64,12 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.current_user(request) is None:
-        abort(403)
-
     if (auth.authorization_header(request) is None
             and auth.session_cookie(request) is None):
         abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
