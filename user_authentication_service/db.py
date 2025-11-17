28c861a03d9d423f9a38e7a3ce41f1566aa056ba
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 11:12:24 +0100

    feat: adding update user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -49,3 +49,12 @@ class DB:
             return user
         except AttributeError:
             raise InvalidRequestError
+
+    def update_user(self, user_id: int, **kwargs) -> User:
+        '''Method to update an user'''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
