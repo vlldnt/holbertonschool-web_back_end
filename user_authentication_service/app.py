a51d54505e59106aedf6e26388cf562a46ce5847
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 15:54:20 +0100

    feat: login method added

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask
 from auth import Auth
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -26,6 +26,20 @@ def post_user():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    '''Login method implemented'''
+    email = request.form.get("email")
+    pwd = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if valid_login:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001")
