a344f409a346921bce24adab024c3ac456df03ef
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 11:56:45 +0100

    fix: short focntion

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -21,13 +21,9 @@ class Auth:
         return bcrypt.hashpw(password.encode('utf-8'), salt)
 
     def register_user(self, email: str, password: str) -> User:
-        """
-        Registers a new user if the email is not already in the db
-        """
-        user = self._db.find_user_by(email=email)
-        if user:
+        """Registers a new user if the email is not already in the db"""
+        try:
+            self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
-
-        hashed_pw = self._hash_password(password)
-        new_user = self._db.add_user(email, hashed_pw)
-        return new_user
+        except NoResultFound:
+            return self._db.add_user(email, self._hash_password(password))
