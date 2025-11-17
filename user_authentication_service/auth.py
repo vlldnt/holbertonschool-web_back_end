f7fe33ab434c4b40447d76e9d08003aaf647d9a0
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 13:21:40 +0100

    fix: trying to find error by adding a return

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -23,7 +23,8 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user if the email is not already in the db"""
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return self._db.add_user(email, self._hash_password(password))
+            user = self._db.add_user(email, self._hash_password(password))
+            return user
         raise ValueError(f"User {email} already exists")
