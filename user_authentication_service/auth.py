f67fb03a5673e5b3109489bacf4e4e8074106fe8
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 14:32:35 +0100

    fix: fix

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,30 +1,52 @@
 #!/usr/bin/env python3
-'''Auth module for session'''
-
+"""authentification system"""
 import bcrypt
+import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """hash password"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ generate uuid"""
+    return str(uuid.uuid4())
+
+
 class Auth:
-    """Auth class with methods
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
-        '''Init method self'''
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        '''Hashed password using bcrypt'''
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password.encode('utf-8'), salt)
 
     def register_user(self, email: str, password: str) -> User:
         """register a user"""
         try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
+            return User
+        raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Get session ID"""
+        try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            user = self._db.add_user(email, self._hash_password(password))
-            return user
-        raise ValueError("User {} already exists".format(email))
+            return
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
