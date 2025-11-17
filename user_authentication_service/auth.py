8adbc4474121d720df143b043e8c5a26f7a858d4
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 11:45:55 +0100

    feat: adding a register method and hashed passworkd

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,9 +2,30 @@
 '''Auth module for session'''
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    '''Hashed password using bcrypt'''
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+class Auth:
+    """Auth class with methods
+    """
+
+    def __init__(self):
+        '''Init method self'''
+        self.db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        '''Hashed password using bcrypt'''
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    def register_user(self, email: str, password: bytes) -> User:
+        '''Register user and return a User'''
+        try:
+            self.db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hash_pw = self._hash_password(password)
+            registered_user = self.db.add_user(email, hash_pw)
+            return registered_user
