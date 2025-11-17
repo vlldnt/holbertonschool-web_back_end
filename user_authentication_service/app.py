c40d76289f036ceb9a8fe6ae9f66aebb6ff9999a
vlldnt <vieilledent.adrien@gmail.com>
2025-11-17 12:20:13 +0100

    feat: adding route to register a new user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask
 from auth import Auth
-from flask import jsonify
+from flask import jsonify, request
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -11,7 +11,20 @@ AUTH = Auth()
 
 @app.route("/", methods=['GET'])
 def bienvenue():
+    '''Home message'''
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route("/users", methods=['POST'])
+def post_user():
+    '''Post a new user'''
+    email = request.form.get("email")
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
