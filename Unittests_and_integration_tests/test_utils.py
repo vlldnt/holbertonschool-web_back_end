cff5d0e7596ef65e61db5e492b6d2a8d5449afbb
vlldnt <vieilledent.adrien@gmail.com>
2025-11-24 10:39:20 +0100

    feat: with execptions

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, "a", KeyError),
+        ({"a": 1}, {"a", "b"}, KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
