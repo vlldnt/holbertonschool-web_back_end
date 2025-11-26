676d9f70ea7ab1d00e4e659987783cc7164ddf14
vlldnt <vieilledent.adrien@gmail.com>
2025-11-26 11:45:20 +0100

    feat: forcing locale to  http://127.0.0.1:5000/?locale=fr or en

---
+++ i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app for i18n project.
+Exposes a single '/' route rendering templates/1-index.html With Babel.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''Class Config with all the config modeule'''
+    LANGUAGES = ["en", "fr"]
+    # BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def get_locale():
+    """Get locale with a default en"""
+
+    user_locale = request.args.get("locale")
+
+    if user_locale and user_locale in app.config["LANGUAGES"]:
+        return user_locale
+
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Render the 3-index.html page"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
