111c247347f7acceee5749aa3a4c0ff3a0490023
vlldnt <vieilledent.adrien@gmail.com>
2025-11-26 10:26:42 +0100

    fix: trying somehting to babel.cfg

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -29,10 +29,8 @@ babel.init_app(app, locale_selector=get_locale)
 
 @app.route("/", methods=["GET"])
 def index():
-    home_title = _("home_title")
-    home_header = _("home_header")
-    """Render the 1-index.html page"""
-    return render_template("3-index.html", home_title=home_title, home_header=home_header)
+    """Render the 3-index.html page"""
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
