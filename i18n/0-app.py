16f23c0913d4349eaae261ffd5634df1d8ba5729
vlldnt <vieilledent.adrien@gmail.com>
2025-11-28 09:48:12 +0100

    doc: doc the class

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -9,6 +9,8 @@ from flask_babel import Babel
 
 
 class Config:
+    '''COnfig class for the app.py'''
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
