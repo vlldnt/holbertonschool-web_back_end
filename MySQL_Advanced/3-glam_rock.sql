2b3457d8a526e3bf79af9bce4311656dd5c83564
vlldnt <vieilledent.adrien@gmail.com>
2025-12-01 14:39:48 +0100

    fix: seriously ? we should put 2024

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (
-        IFNULL(split, YEAR(CURDATE())) - formed
-    ) AS lifespan
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
 FROM metal_bands
 WHERE
     style LIKE '%Glam rock%'
