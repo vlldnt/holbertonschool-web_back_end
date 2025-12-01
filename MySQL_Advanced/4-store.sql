03167a1c43c5b8b19b751cb048ff01277dc08731
vlldnt <vieilledent.adrien@gmail.com>
2025-12-01 15:15:59 +0100

    feat: creating. a new trigger

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- Create a trigger that decreases the quantity of an item after adding a new order
+CREATE TRIGGER decrease_items
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
