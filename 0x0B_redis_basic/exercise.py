5c2ede0a24256438195f307358bddb30bbfb9f8e
vlldnt <vieilledent.adrien@gmail.com>
2025-12-01 10:22:45 +0100

    feat: exercise cache class with store method

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Cache class REDIS"""
+
+from typing import Any
+import redis
+import uuid
+
+
+class Cache():
+    """Cache class Redis"""
+
+    def __init__(self):
+        """Init method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> Any:
+        """Store data with redis cache adn return UUID"""
+        id = str(uuid.uuid4())
+        self._redis.set(id, data)
+        return id
