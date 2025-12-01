2355aa78bcb107262c4ada8693935051a71848b9
vlldnt <vieilledent.adrien@gmail.com>
2025-12-01 14:33:29 +0100

    feat: ranking by longevity rock band

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (
+        IFNULL (split, YEAR (CURDATE ())) - formed
+    ) AS lifespan
+FROM metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
