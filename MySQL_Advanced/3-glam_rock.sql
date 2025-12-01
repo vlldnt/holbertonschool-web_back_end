8ae34a8c9bc7a750cefc8093d4e4f2f4b4ef4e8e
vlldnt <vieilledent.adrien@gmail.com>
2025-12-01 14:36:48 +0100

    fix: space with fucntion du to auto formating

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (
-        IFNULL (split, YEAR (CURDATE ())) - formed
+        IFNULL(split, YEAR(CURDATE())) - formed
     ) AS lifespan
 FROM metal_bands
 WHERE
