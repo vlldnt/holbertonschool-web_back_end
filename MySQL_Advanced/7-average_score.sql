33b8242a2cc3ec26f861d34309871cd47394630e
vlldnt <vieilledent.adrien@gmail.com>
2025-12-02 10:26:34 +0100

    fix: trying some syntax problems with to much spaces

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -4,16 +4,14 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
-
 BEGIN
     UPDATE users
-        SET average_score = (
-            SELECT AVG(score)
-            FROM corrections
-            WHERE user_id = users.id
-        )
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE user_id = users.id
+    )
     WHERE id = user_id;
-
 END$$
 
-DELIMITER;
+DELIMITER ;
