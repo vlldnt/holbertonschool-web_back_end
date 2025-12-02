7d1c4ddb498b6aa96a55b0681b62592453829e71
vlldnt <vieilledent.adrien@gmail.com>
2025-12-02 10:28:26 +0100

    fix: forgot the sorrections name

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -9,7 +9,7 @@ BEGIN
     SET average_score = (
         SELECT AVG(score)
         FROM corrections
-        WHERE user_id = users.id
+        WHERE corrections.user_id = user_id
     )
     WHERE id = user_id;
 END$$
