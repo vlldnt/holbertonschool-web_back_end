43545194bdf05882d8db5ce365c6791771e41e91
vlldnt <vieilledent.adrien@gmail.com>
2025-12-11 13:57:28 +0100

    feat: adding a subcribe to channel aned unsubcribe / quit if message === KILL_SERVER

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import { createClient } from "redis";
+
+const subscriber = createClient();
+
+subscriber.on("error", (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+subscriber.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+
+subscriber.on("message", (channel, message) => {
+  console.log(message);
+  if (message === "KILL_SERVER") {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  }
+});
+
+await subscriber.connect();
+await subscriber.subscribe("holberton school channel");
