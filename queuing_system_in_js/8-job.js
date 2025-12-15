ee93c3385c7fad8a1923ac1cd53771b5b12cf6f4
vlldnt <vieilledent.adrien@gmail.com>
2025-12-15 16:15:46 +0100

    feat: adding a 8-job.js

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,24 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error("Jobs is not an array");
+  }
+
+  jobs.forEach((job) => {
+    const newJob = queue
+      .create("push_notification_code_3", job)
+      .save((error) => {
+        if (!error) {
+          console.log(`Notification job created: ${newJob.id}`);
+        }
+      });
+    newJob.on("complete", () => {
+      console.log(`Notification job ${newJob.id} completed`);
+    });
+    newJob.on("fail", (error) => {
+      console.log(`Notification job ${newJob.id} failed: ${error}`);
+    });
+    newJob.on("progress", (prog) => {
+      console.log(`Notification job ${newJob.id} ${newJob.prog}% complete`);
+    });
+  });
+}
